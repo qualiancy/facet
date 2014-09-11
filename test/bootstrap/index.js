a6dca7574da37e079e859bb706cebda7edca4265
Jake Luer <jake@alogicalparadox.com>
2014-09-11 12:08:17 -0400

    clean: rm -rf coverage/component/browser support

--- test/bootstrap/index.js
+++ test/bootstrap/index.js
@@ -10,26 +10,9 @@ global.should = global.chai.should();
  */
 
 global.chai.use(require('chai-spies'));
-//global.chai.use(require('chai-http'));
 
 /*!
  * Import project
  */
 
 global.facet = require('../..');
-
-/*!
- * Helper to load internals for cov unit tests
- */
-
-function req (name) {
-  return process.env.facet_COV
-    ? require('../../lib-cov/facet/' + name)
-    : require('../../lib/facet/' + name);
-}
-
-/*!
- * Load unexposed modules for unit tests
- */
-
-global.__facet = {};
